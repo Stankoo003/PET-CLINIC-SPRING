8a658b640c02ab0e8e217d42a296aa91e9f39655
stankoo003 <aleksastankovicc.2003@gmail.com>
2022-09-06 11:31:48 +0200

    Adding POJOs. Closes #2.

---
+++ src/main/java/stanko/spring/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package stanko.spring.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/stanko/spring/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package stanko.spring.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
