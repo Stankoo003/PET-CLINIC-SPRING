a2729fbf1903e2b687a9ddc48694bd3cacaf44ad
stankoo003 <aleksastankovicc.2003@gmail.com>
2022-09-15 18:49:44 +0200

    Closes #27

--- pet-clinic-data/src/main/java/stanko/spring/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/stanko/spring/sfgpetclinic/model/Vet.java
@@ -1,4 +1,21 @@
 package stanko.spring.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+
+    private Set<Specialty> specialtySet;
+
+    public Set<Specialty> getSpecialtySet() {
+        return specialtySet;
+    }
+
+    public void setSpecialtySet(Set<Specialty> specialtySet) {
+        this.specialtySet = specialtySet;
+    }
+
+
+
+
 }
