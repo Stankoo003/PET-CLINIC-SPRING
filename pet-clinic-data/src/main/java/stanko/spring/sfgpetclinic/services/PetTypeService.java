32f386ac90f43a579ccaa3476dcce65a2ffe6ed4
stankoo003 <aleksastankovicc.2003@gmail.com>
2022-09-15 19:00:29 +0200

    Closes #29

---
+++ pet-clinic-data/src/main/java/stanko/spring/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package stanko.spring.sfgpetclinic.services;
+
+import stanko.spring.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudServices<PetType,Long> {
+}
---
+++ pet-clinic-data/src/main/java/stanko/spring/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,14 @@
+package stanko.spring.sfgpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import stanko.spring.sfgpetclinic.model.Owner;
+import stanko.spring.sfgpetclinic.model.PetType;
+import stanko.spring.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType,Long>implements PetTypeService<PetType,Long> {
+    @Override
+    public PetType findbyId(Long aLong) {
+        return super.findById(aLong);
+    }
+}
